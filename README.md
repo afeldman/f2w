6a1444197b7affdcc4c974d46ab0d8ff449fd422
anton feldmann <anton.feldmann@aptiv.com>
2020-10-16 13:41:00 +0200

    error things

--- README.md
+++ README.md
@@ -2,16 +2,9 @@
 
 I need a small library to calculate the frequence from a given wavelength and vice versa.
 
-```python
-import f2w 
-
-print f2w.f2w (0.000000001)
-print f2w.w2f (0.000000001)
-```
-
 ## install
-use the requirement.txt to install 
+use the requirement.txt to install
 
 ```bash
-[sudo] pip install -r requirements.txt 
+[sudo] pip install -r requirements.txt
 ```
--- frequency2wavelength/f2w.py
+++ frequency2wavelength/f2w.py
@@ -5,36 +5,64 @@ from scipy import constants as const
 ureg = pint.UnitRegistry()
 
 def funit(x):
-    return {
-        "GHz": 1E9 / ureg.s,
-        "MHz": 1E6 / ureg.s,
-        "KHz": 1E3 / ureg.s,
-        "Hz": ureg.Hz
-    }[x]
+    _units = {
+        "ghz": 1E9 / ureg.s,
+        "mhz": 1E6 / ureg.s,
+        "khz": 1E3 / ureg.s,
+        "hz": ureg.Hz
+    }
+
+    if x in _units.keys():
+        return _units[x]
+    else:
+        print(f"unit {x} not found")
+        return ureg.Hz
+
 
 def wunit(x):
-    return {
+
+    _units = {
         "m": ureg.meter,
         "dm": ureg.dm,
         "cm": ureg.cm,
         "mm": ureg.mm
-    }[x]
+    }
 
-def w2f(wavelength, unit="m", epsilon=np.finfo(float).eps):
+    if x in _units.keys():
+        return _units[x]
+    else:
+        print(f"unit {x} not found")
+        return ureg.meter
+
+def w2f(wavelength, unit=None, epsilon=np.finfo(float).eps):
     if wavelength < epsilon:
         return np.float(0.0)
 
-    l = np.float(wavelength)*wunit(unit)
-    c = np.float(const.speed_of_light)*(ureg.m/ureg.s)
+    _unit = 0
+    if unit is not None:
+        _unit = wunit(unit)
+        c = np.float(const.speed_of_light)*(ureg.m/ureg.s)
+    else:
+        _unit = 1.0
+        c = np.float(const.speed_of_light)
+
+    l = np.float(wavelength)*_unit
 
     return c / l
 
 
-def f2w(frequence, unit="GHz", epsilon=np.finfo(float).eps):
+def f2w(frequence, unit=None, epsilon=np.finfo(float).eps):
     if frequence < epsilon:
         return np.float(0.0)
 
-    f = np.float(frequence)*funit(unit)
-    c = np.float(const.speed_of_light)*(ureg.m/ureg.s)
+    _unit = 0
+    if unit is not None:
+        _unit = wunit(unit)
+        c = np.float(const.speed_of_light)*(ureg.m/ureg.s)
+    else:
+        _unit = 1.0
+        c = np.float(const.speed_of_light)
+
+    f = np.float(frequence)*_unit
 
     return c / f
